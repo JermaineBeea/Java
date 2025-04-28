a8047fd4ca798cc4b22c905b31c92902dd137d81
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-04-29 00:41:16 +0200

    commit

--- Robot3/Direction.java
+++ Robot3/Direction.java
@@ -1,53 +1,42 @@
-public class Direction{
+public class Direction {
+
+    public static void main(String[] args) {
+        DirectionFacing direction = DirectionFacing.NORTH;
+        double unitChange = Movement.FORWARD.getYUnitChange(direction.getCurrentOrientation());
+        System.out.println(unitChange); // Outputs 1.0
+    }
 
     enum DirectionFacing {
-        // Ratio to circumfrence for each direction.
         NORTH(0.25), EAST(0.0), SOUTH(0.75), WEST(0.5);
-    
-        private double currentRatio;
-        Movement FORWARD;
-        Movement BACKWARD;
-        Movement RIGHT;
-        Movement LEFT;
 
-        DirectionFacing(double currentRatio){
-            this.currentRatio = currentRatio;
-            this.FORWARD.setXUnitChange(currentRatio);
-            this.BACKWARD.setYUnitChange(currentRatio);
+        private final double currentOrientation;
+
+        DirectionFacing(double currentOrientation) {
+            this.currentOrientation = currentOrientation;
+        }
+
+        public double getCurrentOrientation() {
+            return currentOrientation;
         }
     }
-    
-    enum Movement{
-        // Mapped to ratio of circumfrence facing east.
+
+    enum Movement {
         FORWARD(0.0), BACKWARD(0.5), RIGHT(0.75), LEFT(0.25);
-    
-        private double defaultRatio;
-        private double xUnitChange;
-        private double yUnitChange;
-    
-        Movement(double defaultRatio){
-            this.defaultRatio = defaultRatio;
+
+        private final double defaultOrientation;
+
+        Movement(double defaultOrientation) {
+            this.defaultOrientation = defaultOrientation;
         }
 
-        public void setXUnitChange(double currentRatio){
-            double radians = 2 * Math.PI * (defaultRatio + currentRatio);
-            xUnitChange = Math.cos(radians);
-        }
-         
-        public void setYUnitChange(double currentRatio){
-            double radians = 2 * Math.PI * (defaultRatio + currentRatio);
-            yUnitChange = Math.sin(radians);
+        public double getXUnitChange(double currentOrientation) {
+            double radians = 2 * Math.PI * (defaultOrientation + currentOrientation);
+            return Math.cos(radians);
         }
 
-        public double getxUnitChange() {
-            return xUnitChange;
-        }
-
-        public double getyUnitChange() {
-            return yUnitChange;
+        public double getYUnitChange(double currentOrientation) {
+            double radians = 2 * Math.PI * (defaultOrientation + currentOrientation);
+            return Math.sin(radians);
         }
     }
-    
 }
-
-

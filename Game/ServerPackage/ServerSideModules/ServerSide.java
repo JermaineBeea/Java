2f693f464f8515598d013970697178de46f55276
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-03 20:52:03 +0200

    commit

---
+++ Game/ServerPackage/ServerSideModules/ServerSide.java
@@ -0,0 +1,9 @@
+package Game.ServerPackage.ServerSideModules;
+
+import java.net.ServerSocket;
+
+import static Game.ServerPackage.ServerSideModules.UtilityFunctions.*;
+
+public class ServerSide {
+    private String IP_ADDRESS = getServerIP();
+}

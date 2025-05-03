093e875f6b376a36029519676bbc519885871f04
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-03 18:45:42 +0200

    commit

--- Robot3/Position/Implementation.java
+++ Robot3/Position/Implementation.java
@@ -5,19 +5,19 @@ public class Implementation {
     public static void main(String[] args) {
 
         Position1 pos1 = new Position1(1, 2);
-        Position3 pos2 = new Position3(1, 2);
-        Position3 pos3 = new Position3(1, 2);
+        Position3 pos2 = new Position3(-1, 2);
+        Position3 pos3 = new Position3(3, -2);
 
         pos1.setDirection(Direction1.SOUTH);
         pos1.moveBackward(4);
         System.out.println(pos1);
 
         pos2.setDirection(Direction3.SOUTH);
-        pos2.moveBackward(4);
+        pos2.moveLeft(3);
         System.out.println(pos2);
 
         pos3.setDirection(Direction3.SOUTH);
-        pos3.moveBackward(4);
+        pos3.moveRight(4);
         System.out.println(pos3);
     }
 }

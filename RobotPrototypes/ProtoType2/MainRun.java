7d3373ec204ca5cedef08ec3e6d5a28ddfb17eb3
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-05 20:10:24 +0200

    commit

--- RobotPrototypes/ProtoType2/MainRun.java
+++ RobotPrototypes/ProtoType2/MainRun.java
@@ -6,7 +6,9 @@ public class MainRun {
         robot.position().setX(3);
         robot.position().setY(4);
         robot.command().forward(8.5);
+        robot.command().rotateLeft(3);
 
         System.out.println("robot is at (" + robot.position().getX() + "," + robot.position().getY() + ")");
+        System.out.println("Robot is facing " + robot.position().getDirection());
     }
 }

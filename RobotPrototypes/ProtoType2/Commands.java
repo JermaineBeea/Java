98f899cfc2e7056050da179831a05d1ed67104ba
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-05 15:29:56 +0200

    commit

--- RobotPrototypes/ProtoType2/Commands.java
+++ RobotPrototypes/ProtoType2/Commands.java
@@ -1,22 +1,36 @@
 public class Commands {
     
     private Position posInstance;
+    int xUnitChange;
+    int yUnitChange;
 
     Commands(Position  instance){
         this.posInstance = instance;
+        xUnitChange = posInstance.getDirection().getXunitChange();
+        yUnitChange = posInstance.getDirection().getYunitChange();
     }
-    
+
+    // Helper functions
+    private void move(double distance, int xUnitChange, int yUnitChange){
+        posInstance.setX(posInstance.getX() + xUnitChange * distance);
+        posInstance.setY(posInstance.getY() + yUnitChange * distance);
+    }
+
+    // Delta functions.
     public void forward(double distance){
-        posInstance.setX(posInstance.getX() + distance);
+        move(distance, xUnitChange, yUnitChange);
     }
+
     public void backward(double distance){
-
+        move(distance, -xUnitChange, -yUnitChange);
     }
+
     public void right(double distance){
-
+        move(distance, yUnitChange, -xUnitChange);
     }
-    public void left(double distance){
 
+    public void left(double distance){
+        move(distance, -yUnitChange, xUnitChange);
     }
 
 }
--- RobotPrototypes/ProtoType2/Direction.java
+++ RobotPrototypes/ProtoType2/Direction.java
@@ -1,12 +1,19 @@
 public enum Direction{
     NORTH(0, 1), EAST(1, 0), SOUTH(0, -1), WEST(-1, 0);
     
-    public final int xUnitChange;
-    
-    public final int yUnitChange;
+    private final int xUnitChange;
+    private final int yUnitChange;
 
     Direction(int xUnitChange, int yUnitChange) {
         this.xUnitChange = xUnitChange;
         this.yUnitChange = yUnitChange;
+    }
+
+    public int getXunitChange(){
+        return xUnitChange;
+    }
+
+    public int getYunitChange(){
+        return yUnitChange;
     }
 }

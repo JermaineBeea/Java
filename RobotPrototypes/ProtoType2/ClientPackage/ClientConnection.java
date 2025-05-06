98d81139735613af6a413397a8c6476966067dc8
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-06 21:38:50 +0200

    commit

--- RobotPrototypes/ProtoType2/ClientPackage/ClientConnection.java
+++ RobotPrototypes/ProtoType2/ClientPackage/ClientConnection.java
@@ -7,12 +7,13 @@ public class ClientConnection {
 
     private static final int SERVER_PORT = 1700;
     private static final String SERVER_IP = "localhost";
+    Game newGame = new Game();
     
     public static void runConnection(){
         System.out.println("Establishing connection to " + SERVER_IP + ":" + SERVER_PORT);
 
         try(Socket serverConnection = new Socket(SERVER_IP, SERVER_PORT)){
-            new Game(serverConnection).run();
+            
         }catch(IOException e){
             System.out.println("Error connecting to server: " + e.getMessage());
         }
--- RobotPrototypes/ProtoType2/ClientPackage/Game.java
+++ RobotPrototypes/ProtoType2/ClientPackage/Game.java
@@ -1,56 +1,25 @@
 package ClientPackage;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
 import java.util.Scanner;
 
 public class Game {
     
-    Boolean isRunning;
-    private Socket serverConnection;
     private final String EXIT_FLAG = "exit";
     private ParseInput inputParser;
-
-    Game(Socket connection){
-        this.serverConnection = connection;
-        this.isRunning = false;
-    }
+    private Scanner consoleIn = new Scanner(System.in);
 
     public void run(){
         System.out.println("\nWelcome to the game, lets begin!");
-        boolean isRunning = true;
-        try(
-            Scanner consoleIn = new Scanner(System.in);
-            DataOutputStream dataToServer = new DataOutputStream(this.serverConnection.getOutputStream());
-            PrintWriter strToServer = new PrintWriter(serverConnection.getOutputStream(), true)
-            ){
-            while(isRunning){
-                try{
-                    System.out.println("\nEnter a command seperated by a space, e.g 'Right 4'");
-                    System.out.println("Type 'exit' to exit game");
-                    String userInput = consoleIn.nextLine();
-                    if(userInput.trim().toLowerCase().equalsIgnoreCase(EXIT_FLAG)){
-                        isRunning = false;
-                        serverConnection.close();
-                    }
-                    inputParser = new ParseInput(userInput);
-                    String command = inputParser.getCommand();
-                    Double quantity = inputParser.getQuantity();
-
-                    // Send data to server.
-                    strToServer.println(command);
-                    dataToServer.writeDouble(quantity);
-
-                    // Wait to reciever server status message.
-
-                }catch(Exception e){
-                    System.out.println(e.getMessage());
-                }
-            }
-        }catch(IOException e){
-            System.out.println("Erro runnning game: " + e.getMessage());
+   
+        System.out.println("\nEnter a command seperated by a space, e.g 'Right 4'");
+        System.out.println("Type 'exit' to exit game");
+        String userInput = consoleIn.nextLine();
+        if(userInput.trim().toLowerCase().equalsIgnoreCase(EXIT_FLAG)){
+            return;
         }
-    }
+        inputParser = new ParseInput(userInput);
+        String command = inputParser.getCommand();
+        Double quantity = inputParser.getQuantity();
+        }
+
 }

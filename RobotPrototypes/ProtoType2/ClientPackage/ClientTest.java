f6688a49f99c6001d757bb77c484e6fdb34a78af
Jermaine <jebeeajhb024.student.wethinkcode.co.za>
2025-05-06 08:59:06 +0200

    commit

---
+++ RobotPrototypes/ProtoType2/ClientPackage/ClientTest.java
@@ -0,0 +1,12 @@
+package ClientPackage;
+
+public class ClientTest {
+    
+    public static void main(String[] args) {
+        
+        Position robPosition = new Position();
+        robPosition.setX(-1);
+        robPosition.setY(-3);
+        robPosition.setDirection(Direction.WEST);
+    }
+}
--- RobotPrototypes/ProtoType2/ServerPackage/Direction.java
+++ RobotPrototypes/ProtoType2/ServerPackage/Direction.java
@@ -14,6 +14,7 @@ public enum Direction{
         this.xUnitChange = xUnitChange;
         this.yUnitChange = yUnitChange;
     }
+    
 
     public int getXunitChange(){
         return xUnitChange;

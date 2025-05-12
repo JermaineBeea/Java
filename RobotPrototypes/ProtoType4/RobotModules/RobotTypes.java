5e896c71f8be319b6193b577d76799031f7b7f3c
Jermaine <jermaine@pop-os.localdomain>
2025-05-12 09:31:38 +0200

    Added Server section

---
+++ RobotPrototypes/ProtoType4/RobotModules/RobotTypes.java
@@ -0,0 +1,15 @@
+package RobotModules;
+import java.util.Set;
+import java.util.HashSet;
+
+public class RobotTypes {
+    
+    private static final Set<Robot> robotTypes = Set.of(
+        new Robot("GermanBullTerrier", 300, 30, 3),
+        new Robot("ItalianKing", 250, 35, 5)
+    );
+
+    public  Set<Robot> getRobotTypes(){
+        return new HashSet<>(robotTypes);
+    }
+}

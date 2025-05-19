593f6128d9cb82ddca661f11030465141ab5122c
Jermaine <jermaine@pop-os.localdomain>
2025-05-19 16:24:10 +0200

    modified

---
+++ PracticeWork/src/main/java/za/co/wethinkcode/Main.java
@@ -0,0 +1,19 @@
+package za.co.wethinkcode;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("level"));
+    }
+
+    static boolean isPalindrome(String string) {
+        StringBuilder str = new  StringBuilder();
+        string.toLowerCase().chars().filter(Character::isAlphabetic).forEach(n-> str.append((char) n));
+        System.out.println( str + ":" + str.reverse());
+        return str == str.reverse();
+    }
+   
+}
+  

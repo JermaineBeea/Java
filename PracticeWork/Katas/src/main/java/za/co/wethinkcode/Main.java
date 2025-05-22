bba1d8aae24b0b044d5646239da2fdf94c199412
Jermaine <jermaine@pop-os.localdomain>
2025-05-22 14:44:06 +0200

    commit

--- PracticeWork/Katas/src/main/java/za/co/wethinkcode/Main.java
+++ PracticeWork/Katas/src/main/java/za/co/wethinkcode/Main.java
@@ -1,10 +1,12 @@
 package za.co.wethinkcode;
 
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(isPalindrome("level"));
+        System.out.println(Character.toUpperCase(0));
     }
 
     static boolean isPalindrome(String string) {
@@ -13,6 +15,28 @@ public class Main {
         System.out.println( str + ":" + str.reverse());
         return str == str.reverse();
     }
+
+    static String toCamelCase(String s){
+        StringBuilder result = new StringBuilder();
+        for(int n = 0; n < s.length(); n++){
+          Character currentChar = s.charAt(n);
+          if(! Character.isAlphabetic(currentChar)){
+            result.append(Character.toUpperCase(s.charAt(n + 1)));
+            n++;
+            continue;
+          }
+          result.append(s.charAt(n));
+        }
+        
+        return result.toString();
+      }
+
+      static String toCamelCase2(String str){
+    String[] words = str.split("[-_]");
+    return Arrays.stream(words, 1, words.length)
+            .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+            .reduce(words[0], String::concat);
+  }
    
 }
   
